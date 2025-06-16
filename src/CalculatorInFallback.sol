286fe99d3d7bd5f39f992211cdfbb7a5261bc7a5
saurabh <saurabhyadav42399@gmail.com>
2025-06-16 19:31:35 +0530

    fmt code

--- src/CalculatorInFallback.sol
+++ src/CalculatorInFallback.sol
@@ -11,8 +11,11 @@ contract CalculatorInFallback {
         // assumming operations won't overflow
 
         // add(uint256,uint256) -> 0x771602f7 (add two numbers and store result in storage)
+        
         // sub(uint256,uint256) -> 0xb67d77c5 (sub two numbers and store result in storage)
+
         // mul(uint256,uint256) -> 0xc8a4ac9c (mul two numbers and store result in storage)
+
         // div(uint256,uint256) -> 0xa391c15b (div two numbers and store result in storage)
     }
 }
